da7a4df8ee366db562f6f9dafe1c101aed0ba043
Sanjay Sah <138980970+sahsanjay3455@users.noreply.github.com>
2024-08-06 17:03:41 +0530

    Time: 0 ms (100%), Space: 7.7 MB (63.9%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string customSortString(string order, string str) 
+    {
+      
+            int count[26] = {0};
+        
+        for(char &x : str)
+            count[x-'a']++;
+        
+        string result = "";
+        for(char &ch : order) {
+            while(count[ch-'a']--) {
+                result.push_back(ch);
+            }
+        }
+        
+        //Add the characters which are not present in 'order'
+        for(char &ch : str) {
+            if(count[ch-'a'] > 0) {
+                result.push_back(ch);
+                count[ch-'a']--;
+            }
+        }
+        
+        return result;
+    }
+        
+    
+};
