29f0016fc66f8a9c583106d24835b7a6c5faeeec
Sanjay Sah <138980970+sahsanjay3455@users.noreply.github.com>
2024-08-24 19:59:39 +0530

    Time: 41 ms (5.25%), Space: 51.3 MB (5.03%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,48 +1,60 @@
 class Solution {
 public:
-    int n;
-    vector<string> result;
-    
-    bool isValid(string str) {
-        if(str[0] == '0')
-            return false;
-        
-        int val = stoi(str);
-        
-        return val <= 255;
+vector<string>result;
+int n;
+
+bool isvalid(string str)
+{
+    if(str[0]=='0')
+    {
+        return false;
     }
-    
-    void solve(string& s, int idx, int part, string curr) {
-        if(idx == n && part == 4) {
-            result.push_back(curr.substr(0, curr.length()-1));
-            return;
-        }
-        
-        if(idx+1 <= n)
-            solve(s, idx+1, part+1, curr + s.substr(idx, 1) + ".");
-        
-        if(idx+2 <= n && isValid(s.substr(idx, 2))) {
-            solve(s, idx+2, part+1, curr + s.substr(idx, 2) + ".");
-        }
-        
-        if(idx+3 <= n && isValid(s.substr(idx, 3))) {
-            solve(s, idx+3, part+1, curr + s.substr(idx, 3) + ".");
-        }
+    int num=stoi(str);
+
+    return num<=255;
+
+}
+   
+   void perform(string s,int idx,int part,string curr)
+   {
+
+    if(part==4 && idx==n)
+    {
+        result.push_back(curr.substr(0,curr.size()-1));
+        return ;
     }
+
+    if(idx+1<=n)
+    {
+        perform(s,idx+1,part+1,curr+s.substr(idx,1)+ ".");
+    }
+
+     if((idx+2<=n) && isvalid(s.substr(idx,2)))
+    {
+        perform(s,idx+2,part+1,curr+s.substr(idx,2)+ ".");
+
+    }
+
+     if((idx+3<=n) && isvalid(s.substr(idx,3)))
+    {
+        perform(s,idx+3,part+1,curr+s.substr(idx,3)+ ".");
+
+    }
+
+   }
     
-    vector<string> restoreIpAddresses(string s) {
+    
+    vector<string> restoreIpAddresses(string s) 
+    {
         result.clear();
-        
-        n = s.length();
-        
-        if(n > 12)
-          return result;
-      
-        int part = 0;
-        string curr = "";
-        
-        solve(s, 0, part, curr);
+    
+        n=s.size();
+        int part=0;
+        string curr="";
+        perform(s,0,part,curr);
         return result;
+
+       
         
     }
 };
