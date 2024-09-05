1d5bbe58982b4fbf773dddbce022ef58cce8acd7
Sanjay Sah <138980970+sahsanjay3455@users.noreply.github.com>
2024-09-06 01:26:49 +0530

    Time: 9 ms (10.26%), Space: 11.8 MB (35.54%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
   // Recursive function to find minimum path sum with memoization
+
     int findMinPath(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
         // Base case: if we are at the bottom row, return the value at this cell
+
         if (i == triangle.size() - 1) return triangle[i][j];
+        
         
         // Check if the result is already computed
         if (dp[i][j] != -1) return dp[i][j];
         
         // Recursively compute the minimum path sum for the current cell
+
         int down = findMinPath(i + 1, j, triangle, dp); // move to the cell directly below
         int diagonal = findMinPath(i + 1, j + 1, triangle, dp); // move to the cell diagonally below
         
