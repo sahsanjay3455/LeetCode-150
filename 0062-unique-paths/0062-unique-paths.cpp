3160245685eac35a488e3ba156fa296a0789ce84
Sanjay Sah <138980970+sahsanjay3455@users.noreply.github.com>
2024-09-06 00:44:56 +0530

    Time: 3 ms (34.17%), Space: 8 MB (20.66%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    // Function to calculate unique paths with memoization
-    int uniquePathsHelper(int i, int j, vector<vector<int>>& dp) 
+int solve(int i,int j,vector<vector<int>>&dp)
+{
+    if(i==0&&j==0)
     {
-        // Base case: if we reach the top-left corner, there's only one way (starting point)
-        if (i == 0 && j == 0) return 1;
-        // If we go out of bounds, no paths are possible
-        if (i < 0 || j < 0) return 0;
-        
-        // Check if the value has already been computed
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        // Recursive relation: paths from above + paths from the left
-        
-        int fromAbove = uniquePathsHelper(i - 1, j, dp);
-        int fromLeft = uniquePathsHelper(i, j - 1, dp);
-        
-        // Store the result in dp table and return it
-        return dp[i][j] = fromAbove + fromLeft;
+        return 1;
     }
+    if(i<0 || j<0)
+    {
+        return 0;
+    }
+    if(dp[i][j]!=-1)return dp[i][j];
 
 
-    int uniquePaths(int m, int n) {
-        // Memoization table initialized with -1
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        // Start from the bottom-right corner
-        return uniquePathsHelper(m - 1, n - 1, dp);
+    int up=solve(i-1,j,dp);
+    int right=solve(i,j-1,dp);
+    return dp[i][j]=(up+right);
+
+}
+    int uniquePaths(int m, int n) 
+    {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+        return solve(m-1,n-1,dp);
+        
     }
 };
