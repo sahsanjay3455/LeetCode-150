df5268161784c8a3acaa4520018d3c27cadb63e4
Sanjay Sah <138980970+sahsanjay3455@users.noreply.github.com>
2024-09-05 20:26:14 +0530

    Time: 2 ms (81.02%), Space: 7.3 MB (72.49%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int fib(int n) {
-        //int a=0,b=1;
-        if(n<=1)
-        {
-            return n;
-        }
-        int first=fib(n-1);
-        int second=fib(n-2);
+        if(n==0)return 0;
+        if(n==1)return 1;
+       
+        int a=0,b=1;
+        int sum=0;
 
-        return (first+second);
+        for(int i=2;i<=n;i++)
+        {
+            sum=(a+b);
+            a=b;
+            b=sum;
+        }
+
+        return sum;
+       
         
     }
 };
