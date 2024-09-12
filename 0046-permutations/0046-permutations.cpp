3bfccf9320480e0dd7ee10d7c13204316ded5c68
Sanjay Sah <138980970+sahsanjay3455@users.noreply.github.com>
2024-09-12 23:12:08 +0530

    Time: 4 ms (43.16%), Space: 9.2 MB (65.13%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-   void permuteHelper(vector<int>& nums, int start, vector<vector<int>>& result) {
-    // Base case: when start index reaches the end of the array
-    if (start == nums.size()) {
-        result.push_back(nums);  // Store the current permutation
-        return;
+void permuteHelper(vector<int>&nums,int indx,vector<vector<int>>&result)
+{
+    if(indx==nums.size())
+    {
+         result.push_back(nums);
+         return ;
     }
-    
-    // Generate permutations by swapping the current index with each subsequent element
-    for (int i = start; i < nums.size(); i++) {
-        swap(nums[start], nums[i]);  // Swap the current element with the start element
-        permuteHelper(nums, start + 1, result);  // Recurse for the next index
-        swap(nums[start], nums[i]);  // Backtrack to restore original order
+    for(int i=indx;i<nums.size();i++)
+    {
+        swap(nums[indx],nums[i]);
+        permuteHelper(nums,indx+1,result);
+        swap(nums[indx],nums[i]);
     }
 }
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+        vector<vector<int>>result;
+       permuteHelper(nums,0,result);
+       return result;
 
-vector<vector<int>> permute(vector<int>& nums) {
-    vector<vector<int>> result;
-    permuteHelper(nums, 0, result);  // Call helper function starting from index 0
-    return result;
-}
-
+    }
+ 
 };
