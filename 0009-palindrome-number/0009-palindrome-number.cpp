ab24a1ff2b629f9857963e13e644c1ef97589a85
Sanjay Sah <138980970+sahsanjay3455@users.noreply.github.com>
2024-09-13 00:33:34 +0530

    Time: 3 ms (93.48%), Space: 8.4 MB (90.19%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        if (x < 0) return false;
+        
+        // Single digit numbers are palindromes
+        if (x >= 0 && x < 10) return true;
+        
+        int num = x;
+        int sum = 0;
+        int rem;
+        
+        // Reverse the number
+        while (num > 0) {
+            rem = num % 10;
+            
+            // Check for overflow before updating sum
+            if (sum > (std::numeric_limits<int>::max() - rem) / 10) {
+                return false;
+            }
+            
+            sum = sum * 10 + rem;
+            num = num / 10;
+        }
+        
+        // Check if the reversed number is the same as the original number
+        return (sum == x);
+    }
+};
